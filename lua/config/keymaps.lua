52859abd35b5b07b409973c4c8cbc349012a3171
Dusan <dusankovacevic47@gmail.com>
2025-11-30 17:31:59 +0000

    Add terminal shortcut

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,16 @@
 
 -- Exit insert mode with jk
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode" })
+
+-- Open horizontal terminal at bottom
+vim.keymap.set("n", "<leader>th", function()
+  vim.cmd("below 15split | terminal")
+end, { desc = "Terminal horizontal" })
+
+-- Open vertical terminal on right
+vim.keymap.set("n", "<leader>tv", function()
+  vim.cmd("vsplit | terminal")
+end, { desc = "Terminal vertical" })
+
+-- Exit terminal mode with jk
+vim.keymap.set("t", "jk", "<C-\\><C-n>", { desc = "Exit terminal mode" })
